bcb164c35409ef054cf68406e5c2aff86fe346f3
Jon Heslop <jon@jonheslop.com>
2021-07-30 11:44:27 +0100

    Max width on titles

--- components/post-title.js
+++ components/post-title.js
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
   return (
-    <h1 className="text-4xl leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className="text-4xl leading-tight md:leading-none mb-12 text-center md:text-left max-w-5xl">
       {children}
     </h1>
   )
